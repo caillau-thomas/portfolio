22512c48b7478844d307789e932ad55ded5b5c5c
Thomas C. <thomas.caillau@gmail.com>
2025-02-28 19:51:56 +0100

    tca-12 (style, feat) Ajout de la div de projet et du bouton de scroll down, style overhaul global

--- src/Components/GlobalStyledComponents.jsx
+++ src/Components/GlobalStyledComponents.jsx
@@ -1,7 +1,14 @@
 import styled from 'styled-components'
 
-export const H1 = styled.h1`
-  font-size: var(--font-size-l);
+export const H3 = styled.h2`
+   font-size: var(--font-size-1s);
+   font-weight: 300;
+   @media (max-width: 1024px) {
+    font-size: var(--font-size-s);
+  }
+`
+export const H4 = styled.h1`
+  font-size: var(--font-size-m);
   font-weight: 500;
   margin: 0;
   @media (max-width: 1024px) {
@@ -9,23 +16,19 @@ export const H1 = styled.h1`
     font-weight: 500;
   }
 `
-
-export const H2 = styled.h1`
-   font-size: var(--font-size-2m);
-   font-weight: 300;
-   margin: 0;
+export const A = styled.a`
+   font-size: var(--font-size-s);
+   font-weight: 500;
+   text-decoration: none;
+   color: var(--color-primary);
    @media (max-width: 1024px) {
-    font-size: var(--font-size-1s);
-    font-weight: 300;
+    font-size: var(--font-size-xs);
+    font-weight: 500;
   }
 `
 
-export const H3 = styled.h2`
-   font-size: var(--font-size-m);
-`
-
-export const A = styled.a`
-   font-size: var(--font-size-s);
+export const A2 = styled.a`
+   font-size: var(--font-size-xs);
    font-weight: 500;
    text-decoration: none;
    color: var(--color-primary);
--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable no-irregular-whitespace */
 import styled from 'styled-components'
-import { H1, H2, A } from './GlobalStyledComponents'
+import { A } from './GlobalStyledComponents'
 import BlurText from "./Animations/BlurTextAnimation";
 import './Animations/style.css'
+import Chevron from '../assets/chevron.png'
 
 const Body = styled.div`
   display: flex;
@@ -21,12 +22,22 @@ const Body = styled.div`
 const Div = styled.div`
   display: flex;
   flex-direction: column;
+  gap:var(--space-s);
+  @media (max-width: 1024px) {
+    gap:var(--space-s);
+  }
+`
+
+const Heading = styled.div`
+  display: flex;
+  flex-direction: column;
   padding-right: var(--space-xl);
   @media (max-width: 1024px) {
     padding-right: 0;
     gap:var(--space-s);
   }
-`
+`;
+
 
 const NavLink = styled.div`
   display: flex;
@@ -40,38 +51,75 @@ const NavLink = styled.div`
   }
 `
 
+const ScrollBtn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: var(--space-1xl);
+  @media (max-width: 1024px) {
+    margin-top: var(--space-xs);
+  }
+`;
+
+
+const ScrollDown = styled.a`
+  display: block;
+  width: 70px; 
+  height: 70px; 
+  position: relative;
+  background-image: url(${Chevron});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  text-align: center;
+  cursor: pointer;
+  @media (max-width: 1280px) {
+    width: 60px; 
+    height: 60px; 
+  }
+  @media (max-width: 1024px) {
+    width: 50px; 
+    height: 50px; 
+  }
+`;
+
 const Landing = () => {
-    const handleAnimationComplete = () => {
-        console.log('Animation completed!');
-    };
-    return (
-        <Body>
-            <Div>
-                <BlurText
-                    text="Hello There 👋"
-                    delay={120}
-                    animateBy="words"
-                    direction="top"
-                    className="H2"
-                    onAnimationComplete={handleAnimationComplete}
-                />
-                <BlurText
-                    text="Moi c’est Thomas, un jeune développeur web passionné en recherche de projets !"
-                    delay={120}
-                    animateBy="words"
-                    direction="top"
-                    className="H1"
-                    onAnimationComplete={handleAnimationComplete}
-                />
-                <NavLink>
-                    <A href="https://www.example.com">👉​ A propos de moi</A>
-                    <A href="https://www.example.com">💻​ Mes Projets</A>
-                    <A href="https://www.example.com">✉️​ Contact</A>
-                </NavLink>
-                <a href="#Projets" class="scroll-down">↓</a>
-            </Div>
-        </Body>
-    )
+  const handleAnimationComplete = () => {
+    console.log('Animation completed!');
+  };
+  return (
+    <Body>
+      <Div>
+        <Heading>
+          <BlurText
+            text="Hello There 👋"
+            delay={120}
+            animateBy="words"
+            direction="top"
+            className="H2"
+            onAnimationComplete={handleAnimationComplete}
+          />
+          <BlurText
+            text="Moi c’est Thomas, un jeune développeur web passionné en recherche de projets !"
+            delay={120}
+            animateBy="words"
+            direction="top"
+            className="H1"
+            onAnimationComplete={handleAnimationComplete}
+          />
+          <NavLink>
+            <A href="https://www.example.com">👉​ A propos de moi</A>
+            <A href="https://www.example.com">💻​ Mes Projets</A>
+            <A href="https://www.example.com">✉️​ Contact</A>
+          </NavLink>
+        </Heading>
+
+        <ScrollBtn>
+          <ScrollDown href="#Projets" className="scroll-down" />
+        </ScrollBtn>
+      </Div>
+    </Body>
+  )
 }
 
 export default Landing
