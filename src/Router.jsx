78e9ec98eaa8037872407617678550e6737cfa5d
Thomas C. <thomas.caillau@gmail.com>
2025-03-20 19:31:15 +0100

    New hotfix try

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import App from './App'
 import { Home } from './Pages/Home'
 
 
 export const Router = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Home />} />
       </Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 )
