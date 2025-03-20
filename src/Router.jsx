0add5f65baea6013e57d35509822ef328e9a2bc4
Thomas C. <thomas.caillau@gmail.com>
2025-03-20 19:38:32 +0100

    new hotfix

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,15 +1,15 @@
-import { HashRouter, BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import App from './App'
 import { Home } from './Pages/Home'
 
 
 export const Router = () => (
-  <HashRouter>
+  <BrowserRouter basename='/portfolio/'>
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Home />} />
       </Route>
     </Routes>
-  </HashRouter>
+  </BrowserRouter>
 )
