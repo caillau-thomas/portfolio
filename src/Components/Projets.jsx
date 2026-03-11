4c7e72f8ed5c4df91ac2a9ed5f520d2485c53408
Thomas C. <thomas.caillau@gmail.com>
2026-03-11 14:03:19 +0100

    Update: ajout d'un nouveau projet

--- src/Components/Projets.jsx
+++ src/Components/Projets.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { H3, H4, A2 } from './GlobalStyledComponents'
 import './Animations/style.css'
 import CF from '../../public/assets/img/Card.png'
+import CF2 from '../../public/assets/img/Card2.png'
 
 const Body = styled.div`
   display: flex;
@@ -22,7 +23,7 @@ const Body = styled.div`
 const Div = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  padding: var(--space-l) var(--space-l);
+  padding: var(--space-l);
   align-items: center;
   justify-content: center;
   gap: 6em;
@@ -43,7 +44,29 @@ const Card = styled.div`
   background-size: contain;
   background-position:  center;
   background-repeat: no-repeat;
-  min-height: 60vh;
+  min-height: 50vh;
+  border-radius: 8px;
+
+  @media (max-width: 1280px) {
+    min-height: 50vh;
+  }
+
+  @media (max-width: 1024px) {
+    min-height: 40vh;
+    gap: var(--space-s);
+  }
+`
+
+const Card2 = styled.div`
+  display: inline-block;
+  gap: var(--space-m);
+  border-radius: var(--radius-medium);
+  background-image: url(${CF2});
+  background-size: contain;
+  background-position:  center;
+  background-repeat: no-repeat;
+  min-height: 50vh;
+  border-radius: 8px;
 
   @media (max-width: 1280px) {
     min-height: 50vh;
@@ -82,9 +105,24 @@ const Projets = () => {
       <Description>
         <H4>Argonauts</H4>
         <H3>Projet de site vitrine a destination d’une organisation de joueurs sur le jeu Star Citizen. </H3>
+        <br />
+        <h3>React</h3>
         <NavLink>
           {/* <A2 href="https://www.example.com">👉​ Découvrez le projet</A2> */}
           <A2 href="https://www.argonauts.fr/" target="_blank">💻​ Visitez le site</A2>
+        </NavLink>
+      </Description>
+      </Div>
+      <Div>
+      <Card2 />
+      <Description>
+        <H4>Lille demain</H4>
+        <H3>Site à destination d’un candidat aux municipales 2026 de la ville de Lille.</H3>
+        <br />
+        <h3>Wordpress</h3>
+        <NavLink>
+          {/* <A2 href="https://www.example.com">👉​ Découvrez le projet</A2> */}
+          <A2 href="https://lilledemain.fr/" target="_blank">💻​ Visitez le site</A2>
         </NavLink>
       </Description>
       </Div>
